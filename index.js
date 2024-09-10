88702ea1c2abe9e981269fff143a37b45b2df12d
ambleandro <ambleandro@gmail.com>
2024-09-10 16:48:58 -0300

    fundamentos 003

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-// arrays, objetos
 let meta = {
   value: 'ler um livro por mês',
-  address: 2;
   ckecked: false,
-  log: (info) => {
-    console.log(info)
-  }
 }
 
-meta.value = "não é mais ler um livro"
-meta.log(meta.value);
+let metas = [
+  meta,
+  {
+    value: "caminhar 20 minutos todos os dias",
+    checked: true,
+  }
+]
 
-// function // arrow function
-//const criarMeta = () => {}
-
-//function criarMeta() {}
+console.log(metas[1].value)
